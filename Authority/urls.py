17525868f33bd32c20e6d9b6406ed9253e9dafe0
Md. Mazharul Islam Antor <43275422+MazharulAntor@users.noreply.github.com>
2020-03-21 23:03:15 +0600

    Sorting Options Added
    Based on skills and experiences individually

--- Authority/urls.py
+++ Authority/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from Authority.views import sorted_list
+from Authority.views import sorted_list_3, home, sorted_list_1, sorted_list_2
 
 urlpatterns = [
-    path('sorted_list/', sorted_list, name='sorted_list')
+    path('sorted_list_1/', sorted_list_1, name='sorted_list_1'),
+    path('sorted_list_2/', sorted_list_2, name='sorted_list_2'),
+    path('sorted_list_3/', sorted_list_3, name='sorted_list_3'),
+    path('home/', home, name='sorted_list')
 ]
--- Authority/views.py
+++ Authority/views.py
@@ -2,9 +2,29 @@ from django.shortcuts import render
 from Candidate.models import Candidate
 
 
-def sorted_list(request):
+def sorted_list_3(request):
     candidates = Candidate.objects.all()
     candidates = list(candidates)
     candidates.sort(key=lambda c: c.experiences_skills_combined_score, reverse=True)
 
     return render(request, "Authority/sorted_list.html", {'candidates': candidates})
+
+
+def sorted_list_2(request):
+    candidates = Candidate.objects.all()
+    candidates = list(candidates)
+    candidates.sort(key=lambda c: c.skills_score, reverse=True)
+
+    return render(request, "Authority/sorted_list.html", {'candidates': candidates})
+
+
+def sorted_list_1(request):
+    candidates = Candidate.objects.all()
+    candidates = list(candidates)
+    candidates.sort(key=lambda c: c.total_experiences, reverse=True)
+
+    return render(request, "Authority/sorted_list.html", {'candidates': candidates})
+
+
+def home(request):
+    return render(request, "Authority/sorted_list.html", {})
